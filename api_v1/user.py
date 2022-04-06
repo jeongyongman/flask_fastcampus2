2a7e5bd126bd251ef7aa9b69a1c9bb37a52682e6
jeongyongman <i1683u@gmail.com>
2022-04-06 15:37:16 +0900

    jQuery와 JWT - 02.jQuery를 활용하여 회원기능 완성하기

--- api_v1/user.py
+++ api_v1/user.py
@@ -6,10 +6,11 @@ from . import api
 @api.route('/users', methods=['GET','POST'])
 def users():
     if request.method == 'POST':
-        userid = request.form.get('userid')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        re_password = request.form.get('re-password')
+        data = request.get_json()
+        userid = data.get('userid')
+        username = data.get('username')
+        password = data.get('password')
+        re_password = data.get('re-password')
 
         if not (userid and username and password and re_password):
             return jsonify({'error': 'No arguments'}), 400
@@ -29,3 +30,31 @@ def users():
 
     users = Fcuser.query.all()
     return jsonify([user.serialize for user in users])
+
+@api.route('/users/<uid>', methods=['GET', 'PUT', 'DELETE'])
+def user_detail(uid):
+    if request.method == 'GET':
+        user = Fcuser.query.filter(Fcuser.id == uid).first()
+        return jsonify(user.serialize)
+    elif request.method == "DELETE":
+        Fcuser.query.delete(Fcuser.id == uid)
+        return jsonify(), 204
+
+    data = request.get_json()
+
+    # userid = data.get('userid')
+    # username = data.get('username')
+    # password = data.get('password')
+
+    # updated_data = {}
+    # if userid:
+    #     updated_data['userid'] = userid
+    # if username:
+    #     updated_data['username'] = username
+    # if password:
+    #     updated_data['password'] = password
+
+    # Fcuser.query.filter(Fcuser.id == uid).update(updated_data)
+    Fcuser.query.filter(Fcuser.id == uid).update(data)
+    user = Fcuser.query.filter(Fcuser.id == uid).first()
+    return jsonify(user.serialize)
