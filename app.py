049ce3139a3b50a5cc83eaf124f0cfbb6ad22f90
jeongyongman <i1683u@gmail.com>
2022-04-07 14:55:10 +0900

    jQuery와 JWT - 05. Flask-JWT를 이용한 인증 시스템 만들기

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
+from flask_jwt import JWT
 from flask import render_template
-from models import db
+from models import Fcuser, db
 from api_v1 import api as api_v1
 
 app = Flask(__name__)
@@ -30,6 +31,13 @@ app.config['SECRET_KEY'] = 'ddd'
 db.init_app(app)
 db.app = app
 db.create_all()
+
+def authenticate(username, password):
+    user = Fcuser.query.filter(Fcuser.userid == username).first()
+    if user.password == password:
+        return user
+
+jwt = JWT(app, authenticate)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
